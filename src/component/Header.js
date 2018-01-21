0430532bd34d6bf92455b32b2f207cd91bc61d77
Rajendra arora <rajendraarora16@yahoo.com>
2018-01-21 16:35:13 +0530

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,6 @@ export default class Header extends React.Component {
         return (
             <div className="header-top">
                 <span>Best dedicated playlist to Rajendra ...</span><br/>
-                <a href="https://www.rajendraarora.com/">https://www.rajendraarora.com</a>
             </div>
         );
     }
